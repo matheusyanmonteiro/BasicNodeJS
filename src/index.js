b17b22944f5bb8e48eef8f022e05536ed252fb39
Matheus Monteiro <matheusyanmonteiro@gamil.com>
2021-03-08 18:18:28 -0300

    created basic routes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({message: "hello world ignite!"});
+app.get("/courses", (request, response) => {
+  return response.json(
+    ["Curso1", "Curso2", "Curso3"]
+    );
+});
+
+app.post("/courses", (request, response) => {
+  return response.json(
+    ["Curso1", "Curso2", "Curso3", "Curso4"]
+  );
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(
+    ["Curso6", "Curso2", "Curso3", "Curso4"]
+  );
+});
+
+app.patch("/courses/:id", (request,response) => {
+  return response.json(
+    ["Curso6", "Curso7", "Curso3", "Curso4"]
+  );
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(
+    ["Curso1", "Curso2", "Curso4"]
+  );
 });
 
 app.listen(3333);
