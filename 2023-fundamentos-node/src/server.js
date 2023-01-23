4d7e4f28d94dd9740e9acb44a99ddcb0c499cdbb
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2023-01-23 14:37:34 -0300

    feat: create the verbs post and get

--- 2023-fundamentos-node/src/server.js
+++ 2023-fundamentos-node/src/server.js
@@ -1,8 +1,28 @@
 import http from 'node:http';
 
+//stateful method for saving data.
+const users = [];
+
 const server = http.createServer((request, response) => {
-  return response.end('hello application');
+  const { method, url } = request;
+
+  if (method === 'GET' && url === '/users') {
+    return response
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users));
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    });
+
+    return response.end('Criaçãõ de usuários');
+  }
+
+  return response.end('index application');
 });
 
 server.listen(3333);
-('');
